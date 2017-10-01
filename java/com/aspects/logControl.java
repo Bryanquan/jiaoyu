ac0abcf73f2c77b60c5bbeb2d03f8af92c053820
Branquan <woaihuangtuo3344>
2017-10-01 16:33:20 +0800

    Add Log Aop Controll

--- java/com/aspects/logControl.java
+++ java/com/aspects/logControl.java
@@ -1,10 +1,36 @@
 package com.aspects;
 
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
+import org.springframework.core.Ordered;
 
 @Aspect
-public class logControl {
+public class logControl implements Ordered {
+
     public logControl() {
     }
 
+    @Pointcut("execution(* com.serviceImpl.*.*(..))")
+    public void logAopController() {
+    }
+
+    @Around("logAopController()")
+    public void log() {
+
+    }
+
+
+    @Before("logAopController()")
+    public void beforeLog() {
+
+    }
+
+
+    @After("logAopController()")
+    public void AfterLog() {
+
+    }
+
+    public int getOrder() {
+        return 0;
+    }
 }
