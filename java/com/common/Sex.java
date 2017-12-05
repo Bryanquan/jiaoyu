b8cb6968e35d4c4708844b867f7af0730b92dac9
Branquan <woaihuangtuo3344>
2017-12-05 10:13:00 +0800

    Format Standardization

---
+++ java/com/common/Sex.java
@@ -0,0 +1,16 @@
+package com.common;
+
+public enum Sex {
+    MALE("M"), FEMALE("F");
+
+    private Sex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    private String sex;
+
+}
--- java/com/serviceImpl/FrontControlServiceImpl.java
+++ java/com/serviceImpl/FrontControlServiceImpl.java
@@ -4,9 +4,10 @@ import com.core.page.Pagination;
 import com.dao.FrontControlDao;
 import com.domain.FrontControl;
 import com.service.FrontControlService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FrontControlServiceImpl implements FrontControlService {
