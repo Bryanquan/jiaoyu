aa5286109f2baf68418060f636547201fccfac1e
Branquan <woaihuangtuo3344>
2017-12-05 10:51:04 +0800

    Format Standardization

--- java/com/common/Sex.java
+++ java/com/common/Sex.java
@@ -3,12 +3,14 @@ package com.common;
 public enum Sex {
     MALE("M"), FEMALE("F");
 
-    private Sex(String sex){
+    private Sex(String sex) {
         this.sex = sex;
     }
-    public String getSex(){
+
+    public String getSex() {
         return sex;
     }
+
     private String sex;
 
 }
